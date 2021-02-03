2324a0650267b36bbfaed3d04840eea23aab5d34
Devid4 <abt.devid@gmail.com>
2021-02-03 21:40:36 +0100

    wrap words to new line

--- src/store/modules/characters.js
+++ src/store/modules/characters.js
@@ -3,9 +3,9 @@ import Status from '../../enums/status'
 
 const state = {
     // text: "Egyszer volt, hol nem volt, volt egyszer egy aranyos, kedves kislány, mindenki szerette, aki csak ismerte, de legjobban mégis a nagymamája. Azt se tudta mi jót adjon neki, mivel keresse kis unokája kedvét. Egyszer egy szép, piros bársonykabátot vett neki ajándékba, ami úgy tetszett a kislánynak, hogy mindig csak ezt hordta. El is nevezték róla Piroskának.",
-    text: "Egyszer volt, hol nem volt, volt egyszer egy aranyos, kedves kislány, mindenki szerette, aki csak ismerte, de legjobban mégis a nagymamája. Azt se tudta mi jót adjon neki, mivel keresse kis unokája kedvét.",
+    // text: "Egyszer volt, hol nem volt, volt egyszer egy aranyos, kedves kislány, mindenki szerette, aki csak ismerte, de legjobban mégis a nagymamája. Azt se tudta mi jót adjon neki, mivel keresse kis unokája kedvét.",
     // text: "Egyszer volt, hol nem volt, volt egyszer egy aranyos, kedves kislány, mindenki szerette",
-    // text: "al fél sakk lék lakk dél sas kas félj kélj éljfal faljlakk dél ék kék séf sék késs adj dal dala dada kaska kakas és a dada dala kél a fajdkakaska fél éljék kékké késés alj fék kék sasfélék késél dajka akad adjad a dajka dala kékké él salakja ajka dala adjasas kas sakk lék fal fél dél él kél ék kék falj adjad dadadala faj kakas kaska a1lakja kajak salak ajakkal kés él séf lék dél kél kajakja alakja salakkal ék fék",
+    text: "al fél sakk lék lakk dél sas kas félj kélj éljfal faljlakk dél ék kék séf sék késs adj dal dala dada kaska kakas és a dada dala kél a fajdkakaska fél éljék kékké késés alj fék kék sasfélék késél dajka akad adjad a dajka dala kékké él salakja ajka dala adjasas kas sakk lék fal fél dél él kél ék kék falj adjad dadadala faj kakas kaska alakja kajak salak ajakkal kés él séf lék dél kél kajakja alakja salakkal ék fék",
     // text: "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99 100",
     characters: [],        // array of the text with placeholders
     currentCharacters: [], // array of currently displayed character
@@ -50,22 +50,24 @@ const actions = {
         }
 
 
-        const regex = /[a-zA-ZöüóőúéáűíÖÜÓŐÚÉÁŰÍ]/
+        const regexL = /[a-zA-ZöüóőúéáűíÖÜÓŐÚÉÁŰÍ]/;
+        const regexR = /[ a-zA-ZöüóőúéáűíÖÜÓŐÚÉÁŰÍ]/;
         for (let i = 20; i < characters.length; i += 20) {
-            if (characters[i-1].char.match(regex) && characters[i].char.match(regex)) {
+            if (characters[i-1].char.match(regexL) && characters[i].char.match(regexR)) {
+                console.log(characters[i-3].char + characters[i-2].char + characters[i-1].char + characters[i].char + characters[i+1].char + characters[i+2].char + characters[i+3].char);
                 console.log(characters[i-1].char + characters[i].char);
                 let startIndex = i - 2;
-                while (characters[startIndex].char === ' ') {
+                while (characters[startIndex].char !== ' ') {
                     startIndex--;
                     if (startIndex % 20 === 0) {
                         break; //TODO skip
                     }
                 }
 
-                console.log(i-startIndex);
+                console.log(i-startIndex-1);
 
-                for (let j = 0; j < i - startIndex; j++) {
-                    characters.splice(startIndex, 0, {
+                for (let j = 0; j < i - startIndex - 1; j++) {
+                    characters.splice(startIndex + 1, 0, {
                         char: '',
                         status: Status.PLACEHOLDER
                     });
@@ -108,9 +110,10 @@ const actions = {
             currentCharacters.push({
                 id: i,
                 char: state.characters[i].char,
-                status: Status.UNTOUCHED
+                status: state.characters[i].status
             })
         }
+
         commit('setCurrentPosition', state.currentPosition - rowsToShift * 20 );
         commit('setNextBatchPosition', state.nextBatchPosition + rowsToShift * 20);
         commit('setCurrentCharacters', currentCharacters);
@@ -128,13 +131,22 @@ const actions = {
                     id: state.currentPosition, 
                     newStatus: Status.CORRECTED 
                 });
+                if (state.currentCharacters[state.currentPosition].char === ' ') {
+                    context.commit('setMissedSpace');
+                }
             }
 
-            if (state.currentPosition + 1 < state.currentCharacters.length) {
-                context.dispatch('updateStatus', { 
-                    id: state.currentPosition + 1, 
-                    newStatus: Status.NEXT 
-                });
+            while (state.currentPosition + 1 < state.currentCharacters.length) {
+                if (state.currentCharacters[state.currentPosition + 1].status === Status.PLACEHOLDER) {
+                    context.commit('setCurrentPosition', state.currentPosition + 1);
+                }
+                else {
+                    context.dispatch('updateStatus', { 
+                        id: state.currentPosition + 1, 
+                        newStatus: Status.NEXT 
+                    });
+                    break;
+                }
             }
 
             context.dispatch('updateProgress');
@@ -164,6 +176,7 @@ const actions = {
 const mutations = {
     setCharacters: (state, characters) => (state.characters = characters),
     setCurrentCharacters: (state, currentCharacters) => (state.currentCharacters = currentCharacters),
+    setMissedSpace: (state) => (state.currentCharacters[state.currentPosition].char = '_'),
     setStatus: (state, {id, newStatus}) => (state.currentCharacters[id].status = newStatus),
     incrementProgress: (state) => {
         state.currentPosition++;
